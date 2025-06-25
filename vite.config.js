dc0a0ce976452296b2dd5f455c471497d0ac2d51
fnmtlocal <fnmtlocal@ET61372.RED.FNMT.RCM>
2025-06-25 12:50:44 +0200

    assets url to docs folder

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-
-  build: {
-    outDir: './docs',
-  }
+    base: '/docs',
+    build: {
+        outDir: './docs',
+    }
 
 });
